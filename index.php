2f94bfbf9b47fc99775a871ddaa8b79af338baaf
airbear901 <wolffaar@gmail.com>
2015-02-28 05:25:56 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Ryan Landau</title>
+	<!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+	<!-- bootstrap -->
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">	
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<link rel="apple-touch-icon-precomposed" href="/logo_iphone.png"/>
+  <link rel="stylesheet" href="css/blueimp-gallery.min.css">
+  <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="http://blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+  <link rel="stylesheet" href="css/bootstrap-image-gallery.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/" style="margin-left: 85px;">RYAN LANDAU</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="collapse">
+      <ul class="nav navbar-nav navbar-right">
+
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact" style="margin-right: 97px;">Contact</a></li>
+        <!-- <li><a href="/videos">Work</a></li> -->
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<!-- header image -->
+<img class="img-responsive" src="images/bannerphoto.jpg" id="main-image">
+
+
+<!-- body container -->
+<div class="container">	
+	<div class="row"><a name="about"></a>
+		<div class="col-xs-12">
+			
+			<h1 id="title">DETROIT-RAISED, TECHNOLOGY-DRIVEN</br>SOCIAL ENTREPRENEUR.</h1>
+		</div>
+		<div class="col-xs-10 col-xs-offset-1">
+			<p>Ryan Landau launched Chalkfly in 2012 where he was the CEO & Co-Founder. Chalkfly 
+
+is an innovative ecommerce company transforming the experience of buying office 
+
+supplies online through technology, customer service, and a Giveback model that is 
+
+improving education in America.</p>
+		</div>
+		<div class="col-xs-10 col-xs-offset-1">
+			<p>Chalkfly has been included among the fastest growing e-commerce companies in Internet Retailer’s Second 500 for both 2013 and 2014. Chalkfly was also ranked by Business Insider as one of the Top 15 Best Startups To Work For in America. On November 
+
+25th 2014, Chalkfly was acquired by Global Office Solutions. Chalkfly investors included Detroit Venture Partners, Ludlow Ventures, Griffon Ventures, Start Garden, 
+
+and Bizdom.</p>
+		</div>
+		<div class="col-xs-10 col-xs-offset-1">
+			<p>Ryan co-founded his first company, Carnival Extravaganza, in 2002, when he was just 
+
+14. Ryan and his brother, Andrew, ran the concession catering company throughout 
+
+high school and college before selling the company to a competitor.</p>
+		</div>
+		<div class="col-xs-10 col-xs-offset-1">
+			<p>Ryan attended the Eli Broad College of Business at Michigan State University where
+
+he studied Supply Chain Management and Entrepreneurship. After graduating Ryan 
+
+moved to Washington, D.C., to work for IBM as a federal consultant.</p>
+		</div>
+		<div class="col-xs-10 col-xs-offset-1">
+			<p style="padding-bottom: 25px;">In addition, Ryan teaches an entrepreneurship class at a local high school and sits on 
+
+the executive board of directors of NextGen, a subsidiary of The Jewish Federation. 
+
+Ryan is a Crain's Detroit 20 in their 20s and won the Young Alumni Achievement 
+
+Award at Michigan State University. As an engaging public speaker, Ryan has led talks 
+
+at TedX, Microsoft and Brand Innovators, as well as universities and businesses 
+
+around the country. He recently completed the Detroit Marathon and his second Ironman competition.</p>
+		</div>
+	</div>
+</div> <!-- container -->
+<div class="container-fluid" id="footer">
+	<div class="col-xs-12">
+		<div class="row"><a name="contact"></a>
+			<h1 style="margin-bottom: 25px;">CONTACT</h1>	    
+		</div>
+		<div class="row" id="social">
+			<a href="https://www.facebook.com/landauryan"><img src="images/facebook.png" id="facebook"></a>
+			<a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=landau.ryan@gmail.com"><img src="images/gmail.png" id="email"></a>
+			<a href="https://www.linkedin.com/in/ryanlandau"><img src="images/linkedin.png" id="linkedin"></a>
+		</div>
+	</div>
+</div> 
+</body>
+</html>
